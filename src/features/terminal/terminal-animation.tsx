79b5be48bd0005ef34c5b6d682b57f0f4f50bbe4
André Victor <victoranalista7@gmail.com>
2025-04-16 23:26:22 -0300

    init

--- src/features/terminal/terminal-animation.tsx
+++ src/features/terminal/terminal-animation.tsx
@@ -1,4 +1,4 @@
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { Button } from "../../components/ui/button";
 import { Avatar, AvatarImage, AvatarFallback } from "../../components/ui/avatar";
 import { FileCheck } from "lucide-react";
